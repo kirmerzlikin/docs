571157a0ed8e431a1e9de3bbf838c917a45aa673
Tomi Virkki <virkki@vaadin.com>
2023-02-15 16:38:25 +0200

    feat: example on how to render react components

---
+++ frontend/demo/component/button/button-basic-react.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+import React from 'react';
+
+export default function ReactExample() {
+    return (
+        <div>
+            <button>I am a Button rendered with React</button>
+        </div>
+    );
+}
